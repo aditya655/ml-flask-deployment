be309ac5b3b8a8868968de05fa2b4817f9987497
Aditya Duggirala <adityaduggirala2001@gmail.com>
2026-08-21 10:28:28 -0700

    Import pickle to load CountVecotrizer and MultinomialNB Classifier ml modles from model folder for checking if email is spam or not.

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request
+import pickle
 
 app = Flask(__name__)
+
+cv = pickle.load(open("model/cv.pkl", "rb"))
+clf = pickle.load(open("model/clf.pkl", "rb"))
+
 
 @app.route("/", methods=["GET","POST"])
 def home():
