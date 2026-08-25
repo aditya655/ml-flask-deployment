ed0a6dd9c29189f8111918a6b2ad83f9c551a236
Aditya Duggirala <adityaduggirala2001@gmail.com>
2026-08-25 10:13:40 -0700

    Another exception for empty content field in client input.

--- app.py
+++ app.py
@@ -28,6 +28,11 @@ def predict_api():
 
   email = data["content"]
 
+  if not email.strip():
+    return jsonify({
+      "error": "content can't be empty"
+    }), 400
+
   prediction =  model_predict(email)
 
   return jsonify({
